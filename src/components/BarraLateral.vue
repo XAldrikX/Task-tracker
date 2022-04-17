5019b1f305c2177f0616ee0d27c55890ac5b6a6a
XAldrikX <nicolasbortoli2010@hotmail.com>
2022-04-17 00:26:39 -0300

    first commit

---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,54 @@
+<template>
+    <header>
+        <h1>
+            <img src="../assets/logoAluraTracker.png"/>
+        </h1>
+        <button class="button" @click="alterarTema">
+            {{ textoBotao }}
+        </button>
+    </header>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue'
+
+    export default defineComponent ({
+        name: 'BarraLateral',
+        emit: ['aoTemaAlterado'],
+        data () {
+            return {
+                modoEscuroAtivo: false
+            }
+        },
+        computed: {
+            textoBotao () {
+                if (this.modoEscuroAtivo) {
+                    return 'Light mode'
+                }
+                return 'Dark mode'
+            }
+        },
+        methods: {
+            alterarTema () {
+                this.modoEscuroAtivo = !this.modoEscuroAtivo
+                this.$emit('aoTemaAlterado', this.modoEscuroAtivo)
+            }
+        }
+     })
+</script>
+
+<style scoped>
+    header {
+        padding: 1rem;
+        background: #0d3b66;
+        width: 100%;
+        height:100vh;
+        text-align: center;
+    }
+    @media only screen and (max-width: 768px) {
+        header {
+            padding: 2.5rem;
+            height: auto;
+        }
+    }
+</style>
